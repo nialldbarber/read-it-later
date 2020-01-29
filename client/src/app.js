1d54c3bd40bb1013158cbfe1b3904db741f40e29
Niall Barber <niall.barber@actual-experience.com>
2020-01-29 11:41:31 +0000

    Added routing

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+// Pages
+import Homepage from '~/pages/homepage'
+import CategoryPage from '~/pages/category-page'
 
 const App = () => (
-  <div>
-    <h1>Hello</h1>
-  </div>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route path="/category/:id" component={CategoryPage} />
+    </Switch>
+  </Router>
 )
 
 export default App
