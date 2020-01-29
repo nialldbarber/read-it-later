1a01164145e86797ef653c3b96de486082c6d302
Niall Barber <niall.barber@actual-experience.com>
2020-01-29 11:44:23 +0000

    Added nav component

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-// Pages
 import Homepage from '~/pages/homepage'
 import CategoryPage from '~/pages/category-page'
+import Nav from '~/components/nav'
 
 const App = () => (
   <Router>
+    <Nav />
     <Switch>
       <Route exact path="/" component={Homepage} />
       <Route path="/category/:id" component={CategoryPage} />
---
+++ client/src/components/nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+const Nav = () => (
+  <header>
+    <Link to="/">
+      <p>LOGO</p>
+    </Link>
+  </header>
+)
+
+export default Nav
