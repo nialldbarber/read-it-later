765698bd4675693fc3582167c6ba471ed4d3fd58
Niall Barber <niall.barber@actual-experience.com>
2020-03-25 18:09:30 +0000

    Addeded in some styling to client

---
+++ client/src/styles/components/card.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.ul`
+  display: grid;
+`;
+
+export const Container = styled.li`
+  padding: 2rem;
+  border: 1px dashed red;
+`;
