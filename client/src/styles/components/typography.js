a939270f098ef014172710ecfe9379f9ca516533
Niall Barber <niall.barber@actual-experience.com>
2020-03-27 11:06:45 +0000

    Added more card styles

---
+++ client/src/styles/components/typography.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Header = styled.h1`
+  font-size: 2.5rem;
+  padding-bottom: 0.5rem;
+  border-bottom: 3px solid ${(props) => props.theme.colours.white};
+`;
