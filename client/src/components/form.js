50a8e2a4f2af07eaec8bf25e50fc9f4f981e43a7
Niall Barber <niall.barber@actual-experience.com>
2020-03-30 10:21:25 +0100

    Added body lock hook

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import SVG from 'react-inlinesvg';
+import useLockBodyScroll from '~/hooks/useLockBodyScroll';
 import { Exit } from '~/styles/components/modals';
 import exit from '~/assets/exit.svg';
 
-const Form = ({ title, submit, name, value, change, buttonText, closeModal }) => (
-  <>
-    <div className="outer" onClick={closeModal} />
-    <div className="inner">
-      <Exit>
-        <SVG src={exit} alt="Exit" aria-label="Exit" onClick={closeModal} />
-      </Exit>
-      <h1>{title}</h1>
-      <form onSubmit={submit} noValidate>
-        <input type="text" name={name} value={value || ''} onChange={change} />
-        <button disabled={value?.length ? false : true} className={value?.length ? '' : 'disabled'}>
-          {buttonText}
-        </button>
-      </form>
-    </div>
-  </>
-);
+const Form = ({ title, submit, name, value, change, buttonText, closeModal }) => {
+  useLockBodyScroll();
+
+  return (
+    <>
+      <div className="outer" onClick={closeModal} />
+      <div className="inner">
+        <Exit>
+          <SVG src={exit} alt="Exit" aria-label="Exit" onClick={closeModal} />
+        </Exit>
+        <h1>{title}</h1>
+        <form onSubmit={submit} noValidate>
+          <input type="text" name={name} value={value || ''} onChange={change} />
+          <button disabled={value?.length ? false : true} className={value?.length ? '' : 'disabled'}>
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Form;
